c76a221db7dd6b107d84d87a943824f5167609dd
Rohit-Ramawat <rohitramawat999@gmail.com>
2023-05-17 13:24:44 +0530

    Map practice

---
+++ Java_Practice/src/com/masai/MapClass.java
@@ -0,0 +1,19 @@
+package com.masai;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapClass {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map = new HashMap<>();
+		
+		map.put(1, "Rohit");
+		map.put(2, "Priyanka");
+		
+		System.out.println(map);
+
+	}
+
+}
